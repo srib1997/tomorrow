cb2cb131e1866bc12614783734cdab2473039ac2
srib <peterf1a08@yahoo.com.hk>
2018-11-24 16:23:07 +0800

    hh

---
+++ tomorrow/SecondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  tomorrow
+//
+//  Created by srib on 23/11/2018.
+//  Copyright © 2018 brainstone. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController,UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tomorrow/ViewController.swift
+++ tomorrow/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+
+    let cellContent = ["Rob", "kirsten", "tommy" , "relphine"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContent.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let arrayObject = UserDefaults.standard.object(forKey: "name")
+        
+        if let araay = arrayObject as? NSArray {
+            
+            print(araay)
+            
+        }
+        
+        UserDefaults.standard.set(cellContent, forKey: "name")
+        
     }
 
 
